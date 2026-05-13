f2943a973bc8744d33b6207af410963b247cbce0
Allen <philallen84@gmail.com>
2026-05-13 14:33:54 +0100

    0.0.40

--- assets/ButtonTabs-LpWzugGS.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-Blucd9Qg.js";import{useMDXComponents as n}from"./index-BRkUXrbv.js";import{M as e,C as i,a as p}from"./index-MsLiSKlD.js";import{B as a,a as s,D as d}from"./ButtonTabs.stories-BEnan5I5.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CPvv__N8.js";import"./index-CbrIw9sE.js";import"./index-Bhelpi4i.js";import"./index-DrFu-skq.js";import"./index-B-lxVbXh.js";import"./v4-CtRu48qb.js";import"./index-eS8l-D1x.js";import"./Tabs-C0t3F5pp.js";import"./utils-Clm8nLOM.js";import"./SelectionIndicator-fmNevtW9.js";import"./useFocusRing-C58TIrH0.js";import"./useEvent-B5MetaT2.js";import"./FocusScope-ozmm2sIM.js";import"./ProgressBar-Dn7Gc5XP.js";import"./Label-DSRs3bn3.js";import"./index-SVU_pXp3.js";import"./useControlledState-fS0KWGMw.js";import"./useListState-Pwm5mX7-.js";import"./Typography-Bg4pE1zg.js";import"./index-NgCCK6aN.js";import"./Button-DzZbFqJb.js";import"./Button-rLog8i9z.js";import"./Spinner-BrD3sMl8.js";function m(r){const o={h1:"h1",h2:"h2",h3:"h3",...n(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
-`,t.jsx(o.h1,{id:"buttontabs",children:"ButtonTabs"}),`
-`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
-`,t.jsx(i,{of:s}),`
-`,t.jsx(p,{of:s}),`
-`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
-`,t.jsx(o.h3,{id:"disabled-items",children:"Disabled items"}),`
-`,t.jsx(i,{of:d})]})}function z(r={}){const{wrapper:o}={...n(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(m,{...r})}):m(r)}export{z as default};
