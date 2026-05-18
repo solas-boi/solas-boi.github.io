2806817f3353b15a16eb0a0fb1123a42b50e972c
Allen <philallen84@gmail.com>
2026-05-18 12:00:11 +0100

    add package

---
+++ assets/ButtonLink-BhFkMIzP.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-Blucd9Qg.js";import{useMDXComponents as s}from"./index-BRkUXrbv.js";import{M as c,C as e,a as d}from"./index-xbQ2TQBS.js";import{B as a,a as i,D as h,N as m,S as p,E as l}from"./ButtonLink.stories-rDlgezWK.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-C9JqA8PX.js";import"./index-CbrIw9sE.js";import"./index-Bhelpi4i.js";import"./index-DrFu-skq.js";import"./index-eS8l-D1x.js";import"./index-CRAeHTmb.js";import"./index-NgCCK6aN.js";import"./index-SVU_pXp3.js";import"./Spinner-BrD3sMl8.js";import"./ProgressBar-Dn7Gc5XP.js";import"./utils-Clm8nLOM.js";import"./Label-DSRs3bn3.js";import"./Button-rLog8i9z.js";import"./useFocusRing-C58TIrH0.js";import"./Typography-Bg4pE1zg.js";function r(t){const o={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(c,{of:a}),`
+`,n.jsx(o.h1,{id:"buttonlink",children:"ButtonLink"}),`
+`,n.jsxs(o.p,{children:[n.jsx(o.code,{children:"ButtonLink"})," is built on the ",n.jsx(o.code,{children:"Button"})," component and is styled to look like a link."]}),`
+`,n.jsxs(o.p,{children:["Select one of ",n.jsx(o.code,{children:"startIcon"})," or ",n.jsx(o.code,{children:"endIcon"})," to see an icon. You'll notice you can't choose both at the same time. Hit the reset button or refresh the page to reset the icon."]}),`
+`,n.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,n.jsx(e,{of:i}),`
+`,n.jsx(d,{of:i}),`
+`,n.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,n.jsx(o.h3,{id:"dashed",children:"Dashed"}),`
+`,n.jsx(e,{of:h}),`
+`,n.jsx(o.h3,{id:"no-underline",children:"No underline"}),`
+`,n.jsx(e,{of:m}),`
+`,n.jsx(o.h3,{id:"start-icon",children:"Start icon"}),`
+`,n.jsx(e,{of:p}),`
+`,n.jsx(o.h3,{id:"end-icon",children:"End icon"}),`
+`,n.jsx(e,{of:l})]})}function F(t={}){const{wrapper:o}={...s(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r(t)}export{F as default};
